02349670c0e96dcc6713e55fde25f300f5487e9e
Ella Rekow <ellarekow@gmail.com>
2023-01-06 22:58:25 -0600

    Updates

---
+++ precache-manifest.af07a074be51496e689660402678307e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23293f8779960127159c668b7bc85606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcc0b0cc4c30f81cc01",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8cd9140e6d5e7ac5c106",
+    "url": "/static/css/main.f8d923e9.chunk.css"
+  },
+  {
+    "revision": "6dcc0b0cc4c30f81cc01",
+    "url": "/static/js/2.dae1b011.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.dae1b011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd9140e6d5e7ac5c106",
+    "url": "/static/js/main.4c004d67.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
