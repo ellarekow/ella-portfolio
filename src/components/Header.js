621afbfb7d9ba22b4a7b80dc45613c3911be0844
Ella Rekow <ellarekow@gmail.com>
2025-12-12 07:48:47 -0600

    Updating header

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,6 +40,7 @@ class Header extends Component {
           <div className="col-md-12">
             <div>
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+              <span className="iconify header-icon" data-icon="la:plane" data-inline="false"></span>
               <br />
               <h1 className="mb-0">
                 <Typical steps={[name]} wrapper="p" />
